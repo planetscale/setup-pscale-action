32a7b32d23d2394fae3a32a779eac759247e55f9
Mike Coutermarsh <coutermarsh.mike@gmail.com>
2023-06-17 08:24:03 -0400

    init action for installing pscale

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,21 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import * as core from '@actions/core';
+import * as tc from '@actions/tool-cache';
+import * as exec from '@actions/exec';
+
+const packageUrl = 'https://github.com/planetscale/cli/releases/download/v0.147.0/pscale_0.147.0_linux_amd64.deb';
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const downloadedPackagePath = await tc.downloadTool(packageUrl);
+    const packagePath = await tc.cacheFile(downloadedPackagePath, 'pscale.deb', 'pscale', '0.147.0', 'deb');
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    await exec.exec('sudo dpkg -i ' + packagePath);
 
-    core.setOutput('time', new Date().toTimeString())
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    if (error instanceof Error) {
+      core.setFailed(error.message);
+    }
   }
 }
 
-run()
+run();
