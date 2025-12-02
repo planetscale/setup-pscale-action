6520bf41077cdece03c94a88e6bea4f08934b5d9
Josh Gross <git@joshmgross.com>
2025-12-02 13:50:48 -0500

    `npm run all`

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,11 @@ import * as tc from '@actions/tool-cache';
 import axios from 'axios';
 
 const linuxPackageUrl =
-  'https://github.com/planetscale/cli/releases/download/{{VERSION}}/pscale_{{VERSION2}}_linux_amd64.tar.gz'
+  'https://github.com/planetscale/cli/releases/download/{{VERSION}}/pscale_{{VERSION2}}_linux_amd64.tar.gz';
 const darwinPackageUrl =
-  'https://github.com/planetscale/cli/releases/download/{{VERSION}}/pscale_{{VERSION2}}_macOS_amd64.tar.gz'
+  'https://github.com/planetscale/cli/releases/download/{{VERSION}}/pscale_{{VERSION2}}_macOS_amd64.tar.gz';
 const windowsPackageUrl =
-  'https://github.com/planetscale/cli/releases/download/{{VERSION}}/pscale_{{VERSION2}}_windows_amd64.zip'
+  'https://github.com/planetscale/cli/releases/download/{{VERSION}}/pscale_{{VERSION2}}_windows_amd64.zip';
 
 async function getLatestReleaseVersion(githubToken?: string): Promise<string> {
   const apiUrl = `https://api.github.com/repos/planetscale/cli/releases/latest`;
@@ -17,14 +17,16 @@ async function getLatestReleaseVersion(githubToken?: string): Promise<string> {
     headers['Authorization'] = `Bearer ${githubToken}`;
   }
 
-  const response = await axios.get(apiUrl, { headers });
+  const response = await axios.get(apiUrl, {headers});
   return response.data.tag_name;
 }
 
 function validateVersion(version: string): void {
   const versionPattern = /^v\d+\.\d+\.\d+$/;
   if (version !== 'latest' && !versionPattern.test(version)) {
-    throw new Error(`Invalid version format: ${version}. Please use the format "vX.X.X" or "latest". See: https://github.com/planetscale/cli/releases for available releases.`);
+    throw new Error(
+      `Invalid version format: ${version}. Please use the format "vX.X.X" or "latest". See: https://github.com/planetscale/cli/releases for available releases.`
+    );
   }
 }
 
@@ -32,7 +34,8 @@ async function run(): Promise<void> {
   let packageUrl = '';
   try {
     const version = core.getInput('version') || 'latest';
-    const githubToken = core.getInput('github-token') || process.env.GITHUB_TOKEN;
+    const githubToken =
+      core.getInput('github-token') || process.env.GITHUB_TOKEN;
 
     validateVersion(version);
 
@@ -61,15 +64,28 @@ async function run(): Promise<void> {
     core.debug(`package url: ${packageUrl}`);
 
     const auth = githubToken ? `Bearer ${githubToken}` : undefined;
-    const downloadedPackagePath = await tc.downloadTool(packageUrl, undefined, auth);
-    const extractedFolder = await tc.extractTar(downloadedPackagePath, 'tools/pscale');
+    const downloadedPackagePath = await tc.downloadTool(
+      packageUrl,
+      undefined,
+      auth
+    );
+    const extractedFolder = await tc.extractTar(
+      downloadedPackagePath,
+      'tools/pscale'
+    );
 
-    const packagePath = await tc.cacheDir(extractedFolder, 'pscale', version === 'latest' ? latestVersion : version);
+    const packagePath = await tc.cacheDir(
+      extractedFolder,
+      'pscale',
+      version === 'latest' ? latestVersion : version
+    );
 
     core.addPath(packagePath);
   } catch (error) {
     if (error instanceof Error) {
-      core.setFailed(`pscale was unable to be installed from GitHub. URL: ${packageUrl}. Error: ${error.message}`);
+      core.setFailed(
+        `pscale was unable to be installed from GitHub. URL: ${packageUrl}. Error: ${error.message}`
+      );
     }
   }
 }
